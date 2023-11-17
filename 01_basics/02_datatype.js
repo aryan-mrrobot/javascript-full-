0b84b2874acfae3efb32cb2f9dbcc9ded40328ae
aryan-mrrobot <aryanrohit9911@gmail.com>
2023-11-17 12:17:04 +0530

    data_types

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3 + 3);
+
+let name = "aryan"
+let age = "21"
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique
+// object
+
+console.log(typeof age)
