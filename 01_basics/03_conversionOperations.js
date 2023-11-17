80bb289e2ebeaaafb6fe559f919777272a1a9d6e
aryan-mrrobot <aryanrohit9911@gmail.com>
2023-11-17 13:55:26 +0530

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 // "33 => 33"
 // "33abc => NaN"
@@ -14,7 +14,7 @@ console.log(valueInNumber)
 let isLoggedIn = "aryan"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /*
 1=>true,0 => false
@@ -24,5 +24,22 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** operations **********************
+
+let value = 3 
+let negValue = -value
+console.log(negValue);
+
+let str1 = "aryan"
+let str2 = " rohit"
+let str3 = str1 + str2
+console.log(str3);
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
